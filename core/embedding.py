4d3728f1fa40ff794b2c0d34ab0baeda09285129
JasontreksC <jasontreks@outlook.com>
2026-05-13 14:34:24 +0900

    test

--- core/embedding.py
+++ core/embedding.py
@@ -127,16 +127,17 @@ f"""
 """
             )
             )
-    es_info_texts = es_info_texts[:100]
-    ss_info_texts = ss_info_texts[:100]
+    es_info_texts = es_info_texts[:50] # for test
+    ss_info_texts = ss_info_texts[:50] # for test
     print(f'임베딩할 총 문서 수: {len(es_info_texts) + len(ss_info_texts)}')
     
     em = EmbeddingManager()
     es_info_vectors = em.embed_texts(es_info_texts)
     ss_info_vectors = em.embed_texts([t[1] for t in ss_info_texts])
-    new_rows = []    
+    new_rows = []
+    hids = list(hids)[:100] # for test
 
-    for i, hid in enumerate(hids[:100]):
+    for i, hid in enumerate(hids):
         es_new_row = VectorStore(
             hid=hid,
             embed_text=es_info_texts[i],
