f74b18944f051d6cc6982aa297ccbdebf5d1d42a
JasontreksC <jasontreks@outlook.com>
2026-05-20 15:11:04 +0900

    chore: 데이터셋 빌드 스크립트

--- test.py
+++ test.py
@@ -1,21 +1,57 @@
 import requests
 import dotenv, os
+import pandas as pd
+from datetime import datetime, timedelta
 
 dotenv.load_dotenv()
 
-class Config:
-    BASE_URL=f'http://{os.getenv('SERVER_HOST')}:{os.getenv('SERVER_PORT')}'
+df_102 = pd.read_csv('./dataset/pm10/PM10_102_180101_251231.csv')
+df_201 = pd.read_csv('./dataset/pm10/PM10_201_180101_251231.csv')
+df_229 = pd.read_csv('./dataset/pm10/PM10_229_180101_251231.csv')
+df_232 = pd.read_csv('./dataset/pm10/PM10_232_180101_251231.csv')
+df_119 = pd.read_csv('./dataset/pm10/PM10_119_180101_251231.csv')
+df_116 = pd.read_csv('./dataset/pm10/PM10_116_180101_251231.csv')
+
+df_102.set_index('일시', inplace=True)
+df_201.set_index('일시', inplace=True)
+df_229.set_index('일시', inplace=True)
+df_232.set_index('일시', inplace=True)
+df_119.set_index('일시', inplace=True)
+df_116.set_index('일시', inplace=True)
 
 
-body = {
-    'text': "상기 환자는 2주 전부터 지속되는 우하복부의 찌르는 듯한 통증과 소화불량을 호소하고 있습니다. 단순 위염보다는 담낭 질환이나 충수돌기염 등 복부 내부 장기의 구조적 이상이 의심되므로, 복부 정밀 영상 촬영(복합 엑스레이 및 단층촬영)과 초음파 검사를 통한 정확한 감별 진단이 요망됩니다."
-}
+current_date = datetime(2018, 1, 1)
+end_date = datetime(2025, 12, 31)
 
-# data_single = {"text": "LangChain과 FastAPI로 임베딩을 테스트합니다."}
-response_post = requests.post(f"{Config.BASE_URL}/search_test", json=body)
+final_dataset = pd.DataFrame(columns=['date', 'point', 'value'])
+final_dataset.set_index('date', inplace=True)
 
-print(f"상태 코드: {response_post.status_code}")
-if response_post.status_code == 200:
-    print(response_post.json())
-else:
-    print(f"에러: {response_post.text}\n")
+while current_date <= end_date:
+    date_string = current_date.strftime('%Y-%m-%d')
+
+    if date_string in df_102.index:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = df_102.at[date_string, '일평균농도']
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = df_102.at[date_string, '지점']
+    elif date_string in df_201.index:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = df_201.at[date_string, '일평균농도']
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = df_201.at[date_string, '지점']
+    elif date_string in df_229.index:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = df_229.at[date_string, '일평균농도']
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = df_229.at[date_string, '지점']
+    elif date_string in df_232.index:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = df_232.at[date_string, '일평균농도']
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = df_232.at[date_string, '지점']
+    elif date_string in df_119.index:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = df_119.at[date_string, '일평균농도']
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = df_119.at[date_string, '지점']
+    elif date_string in df_116.index:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = df_116.at[date_string, '일평균농도']
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = df_116.at[date_string, '지점']
+    else:
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'value'] = -9
+        final_dataset.at[current_date.strftime('%Y%m%d'), 'point'] = -9
+        print(f'{date_string} 날짜에 대한 데이터를 어디서도 구하지 못했음.')
+
+    current_date += timedelta(days=1)
+
+final_dataset.to_csv('./dataset/pm10/pm10_final.csv', encoding='utf-8')
