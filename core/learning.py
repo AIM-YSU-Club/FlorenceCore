9a2c6b96f1f79798cd2b4032d521ba662e3d3fe5
JasontreksC <jasontreks@outlook.com>
2026-05-20 21:21:25 +0900

    feat: 사전 학습 파일 생성 기능을 추가함

--- core/learning.py
+++ core/learning.py
@@ -2,49 +2,61 @@ import pandas as pd
 from pandas import DataFrame
 from sklearn.linear_model import LinearRegression
 import dotenv, os
+import joblib
+from pathlib import Path
 
 dotenv.load_dotenv()
 
 class Config():
     DATASET_PATH=os.getenv('DATASET_PATH')
+    PRETRAINED_PATH=os.getenv('PRETRAINED_PATH')
 
 class Model():
     def __init__(self, target_drug_dataset: str):
-        print('모델 초기화 중...')
-
-        self.df_t = pd.read_csv(f'{Config.DATASET_PATH}/weather/temperature.csv').add_prefix('ta_')
-        self.df_h = pd.read_csv(f'{Config.DATASET_PATH}/weather/humidity.csv').add_prefix('hm_')
-        self.df_r = pd.read_csv(f'{Config.DATASET_PATH}/weather/rainfall.csv').add_prefix('rn_')
-        self.drug_df = pd.read_csv(f'{Config.DATASET_PATH}/{target_drug_dataset}')
-
-        print('데이터셋 불러오기 완료')
+        self.df_ta = pd.read_csv(f'{Config.DATASET_PATH}/kma/temperature.csv').add_prefix('ta_')
+        self.df_hm = pd.read_csv(f'{Config.DATASET_PATH}/kma/humidity.csv').add_prefix('hm_')
+        self.df_rn = pd.read_csv(f'{Config.DATASET_PATH}/kma/rainfall.csv').add_prefix('rn_')
+        self.df_pm = pd.read_csv(f'{Config.DATASET_PATH}/kma/pm10.csv').add_prefix('pm_')
+        self.drug_df = pd.read_csv(f'{Config.DATASET_PATH}/drug/{target_drug_dataset}.csv')
         
-        X = pd.concat([self.df_t, self.df_h, self.df_r], axis=1)
-        X.drop(['ta_ym', 'hm_ym', 'rn_ym'], axis=1, inplace=True)
-
+        X = pd.concat([self.df_ta, self.df_hm, self.df_rn, self.df_pm], axis=1)
         print(f'데이터셋 정보:\n{X.info()}')
+        print('데이터셋 불러오기 완료')
 
+        X.drop(['ta_ym', 'hm_ym', 'rn_ym', 'pm_ym'], axis=1, inplace=True)
         # 결측치(NaN)를 안전하게 0으로 채우기
-        X = X.fillna(0)
+        X.fillna(0, inplace=True)
         Y = self.drug_df['value']
 
-        print('학습 시작...')
 
-        # 모델 생성
-        self.model = LinearRegression()
-        self.model.fit(X, Y)
+        print('모델 초기화 중...')
 
-        print('학습 완료. 모델 생성됨.\n테스트 추론 결과:')
-        predicted_value = self.model.predict(X).round()
+        pt_file_path = Path(f'{Config.PRETRAINED_PATH}/{target_drug_dataset}.pt')
+        if pt_file_path.is_file():
+            self.model = joblib.load(pt_file_path)
+            print('모델 불러오기 완료.')
+        
+        else:
+            print('학습 시작...')
+            # 모델 생성
+            self.model = LinearRegression()
+            self.model.fit(X, Y)
+
+            print('학습 완료. 모델 생성됨.')
+
+        print('[테스트 추론 결과]')
+        predicted_value = self.model.predict(X).round(1)
         result_df = pd.DataFrame({
             '실제 수치 (Actual)': Y,
             'AI 예측 수치 (Predicted)': predicted_value
         })
-        print(result_df.head(10))
+        print(result_df.tail(10))
 
     def getDrugCountMean(self) -> float:
         return self.drug_df['value'].mean()
         
-    def predict(self, ta_4w: list[float], hm_4w: list[float], rn_4w: list[float]) -> float:
-        return float(self.model.predict([ta_4w + hm_4w + rn_4w])[0])
-
+    def predict(self, ta_4w: list[float], hm_4w: list[float], rn_4w: list[float], pm_4w: list[float]) -> float:
+        return float(self.model.predict([ta_4w + hm_4w + rn_4w + pm_4w])[0])
+    
+    def save(self, file_name: str):
+        joblib.dump(self.model, f'{Config.PRETRAINED_PATH}/{file_name}')
